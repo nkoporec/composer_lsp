c98af5777f50aecc689a88540826ac163125fd64
nkoporec <me@nkoporec.com>
2022-11-17 17:35:27 +0100

    Refactor how we handle line parsing and dependency parsing

--- src/composer.rs
+++ src/composer.rs
@@ -30,6 +30,14 @@ pub struct InstalledPackage {
     pub version: String,
 }
 
+#[derive(Deserialize, Debug)]
+struct ComposerJsonFile {
+    require: HashMap<String, String>,
+
+    #[serde(rename(deserialize = "require-dev"))]
+    require_dev: HashMap<String, String>,
+}
+
 pub fn parse_json_file(filepath: Url) -> Option<ComposerFile> {
     let file = Url::parse(&filepath.to_string()).unwrap();
     if file.path().ends_with("composer.json") == false {
@@ -42,59 +50,44 @@ pub fn parse_json_file(filepath: Url) -> Option<ComposerFile> {
         dev_dependencies: Vec::new(),
     };
 
-    let contents = fs::read_to_string(file.path()).expect("Error while reading the composer file");
-
-    let parsed_contents: Value = match serde_json::from_str(&contents) {
-        Ok(v) => v,
-        _ => Value::Null,
-    };
-
-    if parsed_contents.is_null() {
-        return None;
-    }
-
-    let parsed_contents_object = parsed_contents.as_object().unwrap();
-    // Parse line numbers.
-    let require_buffer = parse_by_require_line(file.path());
+    let file_open = File::open(file.path().to_string()).unwrap();
+    let mut reader = BufReader::new(file_open);
+    let composer_json_parsed: ComposerJsonFile =
+        serde_json::from_reader(&mut reader).expect("Can't parse the composer json");
 
     // Get dependencies.
-    if parsed_contents_object.contains_key("require") {
-        let dependencies = &parsed_contents_object["require"];
-        let dep_obj = dependencies.as_object().unwrap();
-        for (name, version) in dep_obj {
-            let line = require_buffer
-                .get(&name.to_string())
-                .expect("Can't unwrap a line num")
-                - 1;
+    for (name, version) in composer_json_parsed.require {
+        let line_num = get_line_num(filepath.path(), "require", &name, version.clone());
 
-            let composer_dependency = ComposerDependency {
-                name: name.to_string(),
-                version: version.to_string(),
-                line,
-            };
+        match line_num {
+            Some(num) => {
+                let composer_dependency = ComposerDependency {
+                    name: name.to_string(),
+                    version: version.to_string(),
+                    line: num,
+                };
 
-            composer_file.dependencies.push(composer_dependency);
+                composer_file.dependencies.push(composer_dependency);
+            }
+            None => {}
         }
     }
 
     // Get dev dependencies.
-    let require_dev_buffer = parse_by_require_dev_line(file.path());
-    if parsed_contents_object.contains_key("require-dev") {
-        let dependencies = &parsed_contents_object["require-dev"];
-        let dep_obj = dependencies.as_object().unwrap();
-        for (name, version) in dep_obj {
-            let line = require_dev_buffer
-                .get(&name.to_string())
-                .expect("Can't unwrap a line num")
-                - 1;
+    for (name, version) in composer_json_parsed.require_dev {
+        let line_num = get_line_num(filepath.path(), "require-dev", &name, version.clone());
 
-            let composer_dependency = ComposerDependency {
-                name: name.to_string(),
-                version: version.to_string(),
-                line,
-            };
+        match line_num {
+            Some(num) => {
+                let composer_dependency = ComposerDependency {
+                    name: name.to_string(),
+                    version: version.to_string(),
+                    line: num,
+                };
 
-            composer_file.dev_dependencies.push(composer_dependency);
+                composer_file.dev_dependencies.push(composer_dependency);
+            }
+            None => {}
         }
     }
 
@@ -165,10 +158,14 @@ pub fn parse_lock_file(composer_file: &ComposerFile) -> Option<ComposerLock> {
     }
 }
 
-fn parse_by_require_line(filepath: &str) -> HashMap<String, u32> {
+fn get_line_num(
+    filepath: &str,
+    block_name: &str,
+    dependency_name: &str,
+    dependency_version: String,
+) -> Option<u32> {
     let file = File::open(filepath);
     let reader = BufReader::new(file.expect("Can't retrieve a file"));
-    let mut buffer: HashMap<String, u32> = HashMap::new();
 
     let mut line_num = 1;
     let mut require_block_start = 0;
@@ -179,69 +176,27 @@ fn parse_by_require_line(filepath: &str) -> HashMap<String, u32> {
         }
 
         let line_text = line.as_ref().expect("Can't unwrap a line text.");
-        if line_text.contains("\"require\": {") {
+        if line_text.contains(&format!("\"{}\":", block_name).to_string()) {
             require_block_start = line_num;
         }
 
         if require_block_start > 0 && line_num > require_block_start {
-            if line_text.contains("},") {
-                break;
+            if line_text.contains(dependency_name) && line_text.contains(&dependency_version) {
+                return Some(line_num);
             }
-
-            let dependency_name_delimiter = line_text
-                .find(":")
-                .expect("Can't find the dependency name delimiter");
-            let dependency_name = &line_text[..dependency_name_delimiter].replace(" ", "");
-            buffer.insert(dependency_name.to_string().replace("\"", ""), line_num);
         }
 
         line_num += 1;
     }
 
-    buffer
-}
-
-fn parse_by_require_dev_line(filepath: &str) -> HashMap<String, u32> {
-    let file = File::open(filepath);
-    let reader = BufReader::new(file.expect("Can't retrieve a file"));
-    let mut buffer: HashMap<String, u32> = HashMap::new();
-
-    let mut line_num = 1;
-    let mut require_block_start = 0;
-    let require_block_end = 0;
-    for line in reader.lines() {
-        if require_block_end > 0 {
-            break;
-        }
-
-        let line_text = line.as_ref().expect("Can't unwrap a line text.");
-        if line_text.contains("\"require-dev\": {") {
-            require_block_start = line_num;
-        }
-
-        if require_block_start > 0 && line_num > require_block_start {
-            if line_text.contains("},") {
-                break;
-            }
-
-            let dependency_name_delimiter = line_text
-                .find(":")
-                .expect("Can't find the dependency name delimiter");
-            let dependency_name = &line_text[..dependency_name_delimiter].replace(" ", "");
-            buffer.insert(dependency_name.to_string().replace("\"", ""), line_num);
-        }
-
-        line_num += 1;
-    }
-
-    buffer
+    None
 }
 
 #[cfg(test)]
 mod tests {
     use reqwest::Url;
 
-    use crate::composer::{parse_json_file, parse_lock_file};
+    use crate::composer::{get_line_num, parse_json_file, parse_lock_file};
 
     #[test]
     fn it_can_parse_a_valid_composer_json_file() {
@@ -258,7 +213,7 @@ mod tests {
         let test_file = Url::from_file_path(format!("{}/tests/composer.json", root_path));
         let parsed_contents = parse_json_file(test_file.unwrap()).unwrap();
 
-        assert_eq!(2, parsed_contents.dependencies.len());
+        assert_eq!(3, parsed_contents.dependencies.len());
     }
 
     #[test]
@@ -278,5 +233,62 @@ mod tests {
         let composer_lock = parse_lock_file(&composer_file);
 
         assert_eq!(83, composer_lock.unwrap().versions.len());
+    }
+
+    #[test]
+    fn it_can_get_the_correct_dependency_line_number() {
+        let root_path = env!("CARGO_MANIFEST_DIR");
+        let test_file = Url::from_file_path(format!("{}/tests/composer.json", root_path)).unwrap();
+
+        let line_number = get_line_num(
+            test_file.path(),
+            "require",
+            "composer/installers",
+            "^2.0".to_string(),
+        )
+        .unwrap();
+
+        assert_eq!(18, line_number);
+    }
+
+    #[test]
+    fn it_can_get_the_correct_dev_dependency_line_number() {
+        let root_path = env!("CARGO_MANIFEST_DIR");
+        let test_file = Url::from_file_path(format!("{}/tests/composer.json", root_path)).unwrap();
+
+        let line_number = get_line_num(
+            test_file.path(),
+            "require-dev",
+            "fake/dependency",
+            "^8.0".to_string(),
+        )
+        .unwrap();
+
+        assert_eq!(25, line_number);
+    }
+
+    #[test]
+    fn it_can_get_the_correct_dependency_line_number_with_same_name() {
+        let root_path = env!("CARGO_MANIFEST_DIR");
+        let test_file = Url::from_file_path(format!("{}/tests/composer.json", root_path)).unwrap();
+
+        let required_dev_line_number = get_line_num(
+            test_file.path(),
+            "require-dev",
+            "fake/dependency",
+            "^8.0".to_string(),
+        )
+        .unwrap();
+
+        let required_line_number = get_line_num(
+            test_file.path(),
+            "require",
+            "fake/dependency",
+            "^8.0".to_string(),
+        )
+        .unwrap();
+
+        assert_eq!(25, required_dev_line_number);
+        assert_eq!(20, required_line_number);
     }
 }
