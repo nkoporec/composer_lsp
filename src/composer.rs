6aafd3f733635116073ab5665a8de3668b4b4b19
nkoporec <me@nkoporec.com>
2022-11-17 09:11:05 +0100

    Fix require-dev dependencies parsing

--- src/composer.rs
+++ src/composer.rs
@@ -55,14 +55,14 @@ pub fn parse_json_file(filepath: Url) -> Option<ComposerFile> {
 
     let parsed_contents_object = parsed_contents.as_object().unwrap();
     // Parse line numbers.
-    let buffer = parse_by_line(file.path());
+    let require_buffer = parse_by_require_line(file.path());
 
     // Get dependencies.
     if parsed_contents_object.contains_key("require") {
         let dependencies = &parsed_contents_object["require"];
         let dep_obj = dependencies.as_object().unwrap();
         for (name, version) in dep_obj {
-            let line = buffer
+            let line = require_buffer
                 .get(&name.to_string())
                 .expect("Can't unwrap a line num")
                 - 1;
@@ -78,11 +78,12 @@ pub fn parse_json_file(filepath: Url) -> Option<ComposerFile> {
     }
 
     // Get dev dependencies.
+    let require_dev_buffer = parse_by_require_dev_line(file.path());
     if parsed_contents_object.contains_key("require-dev") {
         let dependencies = &parsed_contents_object["require-dev"];
         let dep_obj = dependencies.as_object().unwrap();
         for (name, version) in dep_obj {
-            let line = buffer
+            let line = require_dev_buffer
                 .get(&name.to_string())
                 .expect("Can't unwrap a line num")
                 - 1;
@@ -164,7 +165,7 @@ pub fn parse_lock_file(composer_file: &ComposerFile) -> Option<ComposerLock> {
     }
 }
 
-fn parse_by_line(filepath: &str) -> HashMap<String, u32> {
+fn parse_by_require_line(filepath: &str) -> HashMap<String, u32> {
     let file = File::open(filepath);
     let reader = BufReader::new(file.expect("Can't retrieve a file"));
     let mut buffer: HashMap<String, u32> = HashMap::new();
@@ -179,6 +180,42 @@ fn parse_by_line(filepath: &str) -> HashMap<String, u32> {
 
         let line_text = line.as_ref().expect("Can't unwrap a line text.");
         if line_text.contains("\"require\": {") {
+            require_block_start = line_num;
+        }
+
+        if require_block_start > 0 && line_num > require_block_start {
+            if line_text.contains("},") {
+                break;
+            }
+
+            let dependency_name_delimiter = line_text
+                .find(":")
+                .expect("Can't find the dependency name delimiter");
+            let dependency_name = &line_text[..dependency_name_delimiter].replace(" ", "");
+            buffer.insert(dependency_name.to_string().replace("\"", ""), line_num);
+        }
+
+        line_num += 1;
+    }
+
+    buffer
+}
+
+fn parse_by_require_dev_line(filepath: &str) -> HashMap<String, u32> {
+    let file = File::open(filepath);
+    let reader = BufReader::new(file.expect("Can't retrieve a file"));
+    let mut buffer: HashMap<String, u32> = HashMap::new();
+
+    let mut line_num = 1;
+    let mut require_block_start = 0;
+    let require_block_end = 0;
+    for line in reader.lines() {
+        if require_block_end > 0 {
+            break;
+        }
+
+        let line_text = line.as_ref().expect("Can't unwrap a line text.");
+        if line_text.contains("\"require-dev\": {") {
             require_block_start = line_num;
         }
 
@@ -230,7 +267,7 @@ mod tests {
         let test_file = Url::from_file_path(format!("{}/tests/composer.json", root_path));
         let parsed_contents = parse_json_file(test_file.unwrap()).unwrap();
 
-        assert_eq!(1, parsed_contents.dev_dependencies.len());
+        assert_eq!(3, parsed_contents.dev_dependencies.len());
     }
 
     #[test]
